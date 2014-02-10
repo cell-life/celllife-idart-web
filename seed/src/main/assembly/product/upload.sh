e972ce307760a7dedd3f88b920dfec9837932e99
Dagmar Timler <dagmar.timler@gmail.com>
2014-02-10 22:05:24 +0200

    added iDART drugs

--- seed/src/main/assembly/product/upload.sh
+++ seed/src/main/assembly/product/upload.sh
@@ -5,12 +5,14 @@ set -e
 CREDENTIALS="internal:password"
 CONTENT_TYPE_HEADER="Content-Type: application/json"
 
-for FILE in `find $(cd $(dirname $0); pwd;) -name "*.json"`; do
+for FILE in `find $(cd $(dirname $0); pwd;) -name "*.json" | xargs ls`; do
+  echo $FILE
   curl -XPOST \
     -D /tmp/headers \
     -u "${CREDENTIALS}" \
     -d @$FILE \
     -H "${CONTENT_TYPE_HEADER}" \
-    http://localhost:8080/idart/products
+    http://localhost:8080/idart/products || printf '%s\n' $?
   cat /tmp/headers
+  sleep 1
 done
