33fa183708b4bfb00e7e98855be42b714c42e052
Dagmar Timler <dagmar@cell-life.org>
2014-05-27 10:42:18 +0200

    trying a partial revert - restoring test product files

--- seed/src/main/assembly/bootstrap.sh
+++ seed/src/main/assembly/bootstrap.sh
@@ -8,6 +8,7 @@ sh $BASE_DIR/facility/upload.sh
 sh $BASE_DIR/facilityorganisation/upload.sh
 sh $BASE_DIR/organisation/upload.sh
 sh $BASE_DIR/part/upload.sh
+sh $BASE_DIR/product/upload.sh
 sh $BASE_DIR/system/upload.sh
 sh $BASE_DIR/systemfacility/upload.sh
 sh $BASE_DIR/user/upload.sh
